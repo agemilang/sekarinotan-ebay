7ef0c4751c2316cd8e0ca1c3bca7841710a4bc2c
awan gemilang <fredericus.awan@sirclo.com>
2024-10-17 14:05:13 +0700

    init

---
+++ src/Controllers/InspirationController.php
@@ -0,0 +1,11 @@
+<?php
+namespace Sekarinotan\Ebay\Controllers;
+
+use Illuminate\Http\Request;
+
+class InspirationController
+{
+    public function __invoke() {
+        return 'TestLmaoooooo';
+    }
+}
---
+++ src/routes/web.php
@@ -0,0 +1,6 @@
+<?php
+
+use Sekarinotan\Ebay\Controllers\InspirationController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('inspire', InspirationController::class);
